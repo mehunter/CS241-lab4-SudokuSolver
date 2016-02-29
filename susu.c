fe10c759854ac401933d54c76865db1539719292
Matt Hunter <thematthunter@gmail.com>
2016-02-28 18:27:42 -0700

    Now checks for boxes

--- susu.c
+++ susu.c
@@ -1,3 +1,8 @@
+/***************************************/
+/* Matt Hunter -- CS241.01 Spring 2016 */
+/* Lab 4: Sudoku Solver                */
+/***************************************/
+
 /* Set DEBUG to FALSE to perform per spec.  Set to TRUE to run in debug mode */
 #define DEBUG FALSE
 #define TRUE 1
@@ -7,7 +12,7 @@
 
 /* function declarations */
 int readPuzzle(int puzzle[9][9]);
-int validatePuzzle(int puzzle[9][9]);
+int findPuzzleErrors(int puzzle[9][9]);
 void writePuzzle(int puzzle[9][9]);
 int isSafe(int puzzle[9][9], int index, int num);
 int solvePuzzle(int puzzle[9][9], int index);
@@ -20,7 +25,7 @@ int main (void)
 
   while ((error = readPuzzle(puzzle)) != EOF)
   {
-    error += validatePuzzle(puzzle);
+    error += findPuzzleErrors(puzzle);
 
     if (error) printf("Error\n\n");
     else
@@ -65,10 +70,10 @@ int readPuzzle(int puzzle[9][9])
 /* validatePuzzle function verifies that the puzzle is 'well-formed' such that
  * as stated, there are no duplicate entries in any row, column or box.
  */
-int validatePuzzle(int puzzle[9][9])
+int findPuzzleErrors(int puzzle[9][9])
 {
-  int i, row, col;
-
+  int i, j, row, col;
+  
   /* check rows */
   for (row = 0; row < 9; row++)
   {
@@ -78,7 +83,7 @@ int validatePuzzle(int puzzle[9][9])
       {
         if ( (puzzle[row][col] != 0) && (puzzle[row][col] == puzzle[row][i]) )
         {
-          return 1;
+          return TRUE;                      /* row error found in puzzle */
         }
       }
     }
@@ -93,17 +98,34 @@ int validatePuzzle(int puzzle[9][9])
       {
         if ( (puzzle[row][col] != 0) && (puzzle[row][col] == puzzle[i][col]) )
         {
-          return 1;
+          return TRUE;                      /* column error found in puzzle */
         }
       }
     }
   }
 
   /* check boxes */
+  for (row = 0; row < 9; row += 3)
+  {
+    for (col = 0; col < 9; col += 3)
+    {
+      for (i = 0; i < 9; i++)
+      {
+        for (j = i + 1; j < 9; j++)
+        {
+          if ( (puzzle[row + i / 3][col + i % 3] != 0) &&
+               (puzzle[row + i / 3][col + i % 3] ==
+                puzzle[row + j / 3][col + j % 3]) )
+            {
+              return TRUE;                  /* box error found in puzzle */
+            }
+        }
+      }
+    }
+  }
 
-
-  /* no duplicated detected in rows, columns or boxes */
-  return 0;
+  /* no error found in puzzle */
+  return FALSE;
 }
 /* writePuzzle function writes out the sudoku puzzle from the array puzzle[][].
  * If DEBUG is TRUE, then it writes out the puzzle in standard sudoku form. 
